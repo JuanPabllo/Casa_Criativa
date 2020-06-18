f9115a1b1d56aaa07a4269cc80b7bb2a34bacf40
Juan <juanpablo192.dreaw@gmail.com>
2020-06-18 14:34:20 -0300

    Pagina de cadasrto

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.querySelector("button.fat").addEventListener("click", function () {
-    document.querySelector("#footer").classList.toggle("hide")
-})
+function onOff() {
+    document.querySelector("#modal").classList.toggle("hide")
+}
+
